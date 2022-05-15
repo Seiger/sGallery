066f8cbea0191526e2fa7a578fdcd31994573348
Serhii Kornelyuk <seiger@meta.ua>
2022-05-15 22:45:21 +0300

    [FIX] Auto migration.

--- database/migrations/2022_05_15_185152_create_s_gallery_table.php
+++ database/migrations/2022_05_15_185152_create_s_gallery_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSGallerysTable extends Migration
+class CreateSGalleryTable extends Migration
 {
     /**
      * Run the migrations.
