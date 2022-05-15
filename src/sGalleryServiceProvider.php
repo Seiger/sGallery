80ac116483a2b46152d2e408df045666ae285f0d
Serhii Kornelyuk <seiger@meta.ua>
2022-05-15 23:28:05 +0300

    [FIX] Auto migration.

--- src/sGalleryServiceProvider.php
+++ src/sGalleryServiceProvider.php
@@ -14,7 +14,16 @@ class sGalleryServiceProvider extends ServiceProvider
             include(__DIR__.'/Http/routes.php');
 
             //Migration for create tables
-            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+            //$this->loadMigrationsFrom(__DIR__ . '/../database/migrations'); 2022_05_15_185152_create_s_gallery_table.php
+            if ($this->app->runningInConsole()) {
+                // Export the migration
+                if (!class_exists('CreateSGalleryTable')) {
+                    $this->publishes([
+                        __DIR__ . '/../database/migrations/create_s_gallery_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_s_gallery_table.php'),
+                        // you can add any number of migrations here
+                    ], 'migrations');
+                }
+            }
 
             //Views
             $this->loadViewsFrom(__DIR__ . '/../views', 'sGallery');
