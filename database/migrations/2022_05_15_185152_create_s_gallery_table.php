f2505a5967ca591e7e9c47884bb159afaf83da0c
Serhii Korneliuk <info@seigerit.com>
2022-05-15 20:12:36 +0300

    [FIX] Migrations.

--- database/migrations/2022_05_15_185152_create_s_gallery_table.php
+++ database/migrations/2022_05_15_185152_create_s_gallery_table.php
@@ -1,4 +1,4 @@
-<?php namespace Seiger\sGallery\Migration;
+<?php namespace Seiger\sGallery\Database\Migrations;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
