cd7102b1fe9ad53100940d9b1ff5ddadf20c3b4d
Serhii Korneliuk <info@seigerit.com>
2022-06-01 13:16:12 +0300

    [FIX] Migration.

--- plugins/sGalleryPlugin.php
+++ plugins/sGalleryPlugin.php
@@ -1,7 +1,7 @@
 <?php
 
 Event::listen('evolution.OnDocFormRender', function($params) {
-    if (in_array($params['template'], evo()->getConfig('sgallery', [0])) && $params['id'] > 0) {
+    if (in_array($params['template'], evo()->getConfig('sGallery', [0])) && $params['id'] > 0) {
         return sGallery::initialise();
     }
 });
